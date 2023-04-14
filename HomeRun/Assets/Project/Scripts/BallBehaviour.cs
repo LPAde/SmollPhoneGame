728ada17f4e1da000d54fd0d4d46d9005707bfb1
LPAde <andre-peters01@hotmail.de>
2023-04-14 20:06:37 +0200

    added base start

--- HomeRun/Assets/Project/Scripts/BallBehaviour.cs
+++ HomeRun/Assets/Project/Scripts/BallBehaviour.cs
@@ -6,6 +6,10 @@ public class BallBehaviour : MonoBehaviour
 {
     public static BallBehaviour Instance;
 
+    [SerializeField] private GameObject ballFollowCam;
+    [SerializeField] private Vector2 startVector;
+    [SerializeField] private Vector2 startVelocity;
+
     [Header("Properties")]
     [SerializeField] private Rigidbody2D rigid;
 
@@ -17,9 +21,33 @@ public class BallBehaviour : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        startVector = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            rigid.gravityScale = 0;
+            transform.position = startVector;
+            ballFollowCam.SetActive(false);
+        }
+
+        if (rigid.gravityScale == 1)
+            return;
+
+        rigid.velocity = startVelocity;
+
+        if (transform.position.x < -1)
+            transform.position = startVector;
+    }
+
     public void Initialize()
     {
         rigid.gravityScale = 1;
+        ballFollowCam.SetActive(true);
     }
 
     /// <summary>
